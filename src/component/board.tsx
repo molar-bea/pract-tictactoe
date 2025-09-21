7c9dce7410cf67e17414d33742d4a69e9fe16f39
molar-bea <molarbea123@gmail.com>
2025-09-21 23:27:30 +0800

    add winner status

--- src/component/board.tsx
+++ src/component/board.tsx
@@ -5,19 +5,34 @@ export default function Board(): JSX.Element {
   const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
 
+
  function handleClick(i: number): void {
-    if (squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+  if (calculateWinner(squares) || squares[i]) {
+    return;
   }
+
+  const nextSquares: (string | null)[] = squares.slice();
+
+  if (xIsNext) {
+    nextSquares[i] = 'X';
+  } else {
+    nextSquares[i] = 'O';
+  }
+
+  setSquares(nextSquares);
+  setXIsNext(!xIsNext);
+}
+
+
+const winner: string | null = calculateWinner(squares);
+let status: string;
+
+if (winner) {
+  status = 'Winner: ' + winner;
+} else {
+  status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+}
+
 
 
 
@@ -40,4 +55,26 @@ export default function Board(): JSX.Element {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares: (string | null)[]): string | null {
+  const lines: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
